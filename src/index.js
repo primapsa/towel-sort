69e6164ce3261083b13c1a50aee755219bd8f180
Siarhei Prymakovich <prima.psa@gmail.com>
2021-03-11 00:36:12 +0300

    feat: implement towel-sort fnc

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  
+  let result = [];
+  if(matrix && matrix.length > 0){
+    
+     result = matrix.reduce(function(acc,item,index){
+      if (index%2 !== 0) item.reverse()
+      return acc.concat(item);
+    }
+    )
+  }
+  return result;
 }
